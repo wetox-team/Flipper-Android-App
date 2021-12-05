7c27c2505b007b479f3aa8de24126ecfb98af919
LionZXY <nikita@kulikof.ru>
2021-12-05 14:41:42 +0300

    Add debug rpc button

--- components/info/impl/src/main/java/com/flipperdevices/info/impl/main/viewmodel/InfoViewModel.kt
+++ components/info/impl/src/main/java/com/flipperdevices/info/impl/main/viewmodel/InfoViewModel.kt
@@ -1,13 +1,18 @@
 package com.flipperdevices.info.impl.main.viewmodel
 
 import androidx.lifecycle.viewModelScope
+import com.ezhevita.protobuf.telegram.telegramDialog
+import com.ezhevita.protobuf.telegram.telegramMessage
+import com.ezhevita.protobuf.telegram.telegramStateResponse
 import com.flipperdevices.bridge.api.model.FlipperGATTInformation
+import com.flipperdevices.bridge.api.model.wrapToRequest
 import com.flipperdevices.bridge.service.api.FlipperServiceApi
 import com.flipperdevices.bridge.service.api.provider.FlipperBleServiceConsumer
 import com.flipperdevices.bridge.service.api.provider.FlipperServiceProvider
 import com.flipperdevices.core.di.ComponentHolder
 import com.flipperdevices.core.ui.LifecycleViewModel
 import com.flipperdevices.info.impl.di.InfoComponent
+import com.flipperdevices.protobuf.main
 import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -33,6 +38,29 @@ class InfoViewModel : LifecycleViewModel(), FlipperBleServiceConsumer {
 
     fun getConnectionState(): StateFlow<ConnectionState> {
         return connectionState
+    }
+
+    fun sendTestCommand() {
+        bleService.provideServiceApi(this) {
+            it.requestApi.request(
+                main {
+                    telegramStateResponse {
+                        dialogs.add(
+                            telegramDialog {
+                                id = 1
+                                name = "Test"
+                                messages.add(
+                                    telegramMessage {
+                                        text = "Test Message"
+                                        isOur = true
+                                    }
+                                )
+                            }
+                        )
+                    }
+                }.wrapToRequest()
+            ).launchIn(viewModelScope)
+        }
     }
 
     override fun onServiceApiReady(serviceApi: FlipperServiceApi) {
